cc7b42912fcb484954dbdd062e17a938ac99f580
Jonas Enge <jonas.enge@gmail.com>
2018-06-15 14:02:22 +0200

    Genesis commit

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ module.exports = {
   MOTTAK_SERVICE_USERNAME: process.env.MOTTAK_SERVICE_USERNAME || 'username',
   MOTTAK_SERVICE_PASSWORD: process.env.MOTTAK_SERVICE_PASSWORD || 'password',
   MOTTAK_SERVICE_KEY: process.env.MOTTAK_SERVICE_KEY || `${process.cwd()}/certs/key.pem`,
+  MOTTAK_SERVICE_KVITTER_URL: process.env.MOTTAK_SERVICE_KVITTER_URL || 'https://svarut.ks.no/tjenester/svarinn/kvitterMottak/forsendelse',
   JOBS_DIRECTORY_PATH: process.env.JOBS_DIRECTORY_PATH || 'test/directories/jobs',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'robot-mottak-retrive-files',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
